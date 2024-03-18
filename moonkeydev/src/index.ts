babae1c72271251ccdfb28422eb5722d76e02a75
BrunaCardoso7 <obitadrawing@gmail.com>
2024-03-18 13:02:18 -0300

    fix: remove instance in intialize appdatasource

--- moonkeydev/src/index.ts
+++ moonkeydev/src/index.ts
@@ -3,15 +3,6 @@ import { User } from "./User/user.entity"
 
 AppDataSource.initialize().then(async () => {
 
-    console.log("Inserting a new user into the database...")
-    const user = new User()
-    user.nome = "bruna fernandes"
-    user.telefone = "86994193056"
-    user.email = "brunafernandes@gmail.com"
-    user.password = "bruna123"
-    await AppDataSource.manager.save(user)
-    console.log("Saved a new user with id: " + user.id)
-
     console.log("Loading users from the database...")
     const users = await AppDataSource.manager.find(User)
     console.log("Loaded users: ", users)
